e37eec6aef95de32010e35714c3b321e8174bcab
Ethel <134110862+ethelbeluzzi@users.noreply.github.com>
2025-08-08 23:01:03 -0300

    Update test_preprocess.py

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -2,6 +2,6 @@ from src.preprocess import normalize
 
 # Deve remover acentos, colocar em minúsculas, tirar HTML e espaços extras
 def test_normalize_removes_accents_and_html():
-    texto = "  Eu <b>AMO</b> escrever!   "
-    esperado = "eu amo escrever!"
+    texto = "  Eu <b>AMO</b> escrever épicos da literatura!   "
+    esperado = "eu amo escrever epicos da literatura!"
     assert normalize(texto) == esperado
